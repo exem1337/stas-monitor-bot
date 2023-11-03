d91ae807e948e73240dd412fb604537bfce9b892
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-11-03 20:35:56 +0500

    da

--- src/views/Chart.tsx
+++ src/views/Chart.tsx
@@ -24,7 +24,7 @@ const Chart = (props) => {
     });
   return (
     <div className={styles.container__bloc}>
-      { tg?.ThemeParams}
+      { JSON.stringify(tg?.ThemeParams) }
       <ResponsiveContainer
         width="100%"
         height={200}
