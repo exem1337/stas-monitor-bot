b3809cc2bc872e86836001603b6b406769e2ec81
Air1K <veremeenkom91@gmail.com>
2023-11-04 02:40:49 +0500

    Добавил блок с выводом статуса

--- src/views/MyDB.tsx
+++ src/views/MyDB.tsx
@@ -1,85 +1,109 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import {Accordion, Container, ProgressBar, Spinner} from "react-bootstrap";
 import {useTelegram} from '../hooks/useTelegram'
 import Chart from "./Chart";
 import BaseButton from "../components/ui/BaseButton/BaseButton";
-import { IDatabase } from "../models/db.model";
-import { DBApi } from "../services/dbApiService";
+import {IDatabase, IDatabaseHost} from "../models/db.model";
+import {DBApi} from "../services/dbApiService";
+import {DB_STATUS_NAME_MAP} from "../constants/dbStatusNameMap.const";
+import {EDBStatuses} from "../enums/dbStatuses.enum";
 
 const MyDB = () => {
-  const { id } = useParams()
-  const [database, setDatabase] = useState<IDatabase>({} as IDatabase);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+   const {id} = useParams()
+   const [database, setDatabase] = useState<IDatabase>({} as IDatabase);
+   const [isLoading, setIsLoading] = useState<boolean>(true);
+   // const [dbs, setDbs] = useState<Array<IDatabaseHost>>([]);
+   const getDbInfo = async () => {
+      setIsLoading(true);
+      setDatabase(await DBApi.getDb(Number(id)));
+      setIsLoading(false);
+   }
 
-  const getDbInfo = async () => {
-    setIsLoading(true);
-    setDatabase(await DBApi.getDb(Number(id)));
-    setIsLoading(false);
-  }
+   useEffect(() => {
+      getDbInfo();
+   }, [])
 
-  useEffect(() => {
-    getDbInfo();
-  }, [])
+   const db = {
+      id: 1,
+      name: "Имя",
+      data: [
+         {
+            name: 'Имя1', time: Date.now(), 'Сбои': "300",
+         },
+         {
+            name: 'Имя2', time: Date.now() - 10000, 'Сбои': "700",
+         },
+         {
+            name: 'Имя3', time: Date.now() - 18000, 'Сбои': "500",
+         },
+         {
+            name: 'Имя4', time: Date.now() - 35000, 'Сбои': "100",
+         },
+      ]
+   };
+   const {tg} = useTelegram();
 
-  const db = {
-    id: 1,
-    name: "Имя",
-    data: [
-       {
-         name: 'Имя1', time: Date.now() , 'Сбои': "300",
-       },
-      {
-        name: 'Имя2', time: Date.now() - 10000, 'Сбои': "700",
-      },
-      {
-        name: 'Имя3', time: Date.now() - 18000, 'Сбои': "500",
-      },
-      {
-        name: 'Имя4', time: Date.now() - 35000, 'Сбои': "100",
-      },
-    ]
-};
-  const { tg } = useTelegram();
-  
-  useEffect(() => {
-    tg.ready();
-  }, []);
+   useEffect(() => {
+      tg.ready();
+   }, []);
 
-  const now = 60;
+   const now = 60;
 
-  if (isLoading) {
-    return (
-      <div className="app-loader">
-        <Spinner />
-      </div>
-    )
-  }
+   if (isLoading) {
+      return (
+         <div className="app-loader">
+            <Spinner/>
+         </div>
+      )
+   }
 
-  return (
-    <Container className="my-db">
-      <h1>{database.name}</h1>
-      <BaseButton text="Назад" />
+   const getColor = () => {
+      let color = '';
+      switch (database.state){
+         case EDBStatuses.Online:
+            color = 'green';
+            break
+         case EDBStatuses.Offline:
+            color = 'yellow';
+            break
+         case EDBStatuses.Error:
+            color = 'red';
+            break
+         default: color = 'red';
+      }
+      return {borderColor: color, color: color}
+   }
 
-      <span>Занято дискового пространства</span>
-      <ProgressBar now={now} label={`${now}%`} />
-      
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Количество активных сессий</Accordion.Header>
-          <Accordion.Body>
-            <Chart data={db.data}/>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Количество транзакций</Accordion.Header>
-          <Accordion.Body>
-            <Chart data={db.data}/>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </Container>
-  );
+   return (
+      <Container className="my-db">
+         <div className={'d-flex align-items-center'}>
+            <h1 className={'mb-2'}>
+               <span>{database.name}</span>
+            </h1>
+            <span className={'ms-2 my-db--status'} style={getColor()}>{DB_STATUS_NAME_MAP.get(database.state)}</span>
+         </div>
+         <BaseButton text="Назад"/>
+
+         <span>Занято дискового пространства</span>
+         <ProgressBar now={now} label={`${now}%`}/>
+
+         <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+               <Accordion.Header>Количество активных сессий</Accordion.Header>
+               <Accordion.Body>
+                  <Chart data={db.data}/>
+               </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+               <Accordion.Header>Количество транзакций</Accordion.Header>
+               <Accordion.Body>
+                  <Chart data={db.data}/>
+               </Accordion.Body>
+            </Accordion.Item>
+         </Accordion>
+      </Container>
+   );
 };
 
 export default MyDB;
