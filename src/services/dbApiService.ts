b1903ecc4d72591d73f1368067fa7b4d467c6fe5
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-11-03 23:22:28 +0500

    dawd

--- src/services/dbApiService.ts
+++ src/services/dbApiService.ts
@@ -2,7 +2,7 @@ import api from '../http';
 
 export class DBApi {
   public static async getAllDbs(telegramId: number) {
-    return await api.get(`/monitoring/dbList/${telegramId}`)
+    return await api.get(`/fullHostsDbList/${telegramId}`)
   }
 
   public static async createConnection(connection) {
