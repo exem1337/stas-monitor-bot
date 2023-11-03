8a19bcd8db2aeba00eb6eb361efeef8c16ad11c9
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-11-03 16:56:23 +0500

    user data

--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -1,20 +1,22 @@
 import DBListItem from "../components/DBListItem"
 import { EDBStatuses } from "../enums/dbStatuses.enum"
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTelegram } from "../hooks/useTelegram";
 
 const MainPage = () => {
   const listId: Array<number> = [3,4,5,6,7,788];
   const { onToggleButton, tg } = useTelegram();
+  const [userData, setUserData] = useState('');
   
   useEffect(() => {
     tg.ready();
     console.log(tg.initData, tg.initDataUnsafe)
+    setUserData(JSON.stringify(tg.initData));
   }, [])
 
   return (
     <div className="main-page">
-      {tg.initDataUnsafe?.user?.username}
+      {userData}
       { listId.map((id) => 
           <DBListItem 
             id={id} 
