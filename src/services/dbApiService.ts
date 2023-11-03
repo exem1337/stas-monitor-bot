f8c030424d5187718794a8d4eb1ab461d3806da8
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-11-04 01:43:52 +0500

    dawadw dwad

--- src/services/dbApiService.ts
+++ src/services/dbApiService.ts
@@ -2,7 +2,7 @@ import api from '../http';
 
 export class DBApi {
   public static async getAllDbs(telegramId: string) {
-    return await api.get('https://jsonplaceholder.typicode.com/todos/1')
+    return await api.get('https://jsonplaceholder.typicode.com/posts')
   }
 
   public static async createConnection(connection) {
