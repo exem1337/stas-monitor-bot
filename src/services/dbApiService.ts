f539781effc94d8d3b5ac1ec5c71521a2fd08528
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-11-03 23:25:57 +0500

    fix conection

--- src/services/dbApiService.ts
+++ src/services/dbApiService.ts
@@ -2,7 +2,7 @@ import api from '../http';
 
 export class DBApi {
   public static async getAllDbs(telegramId: number) {
-    return await api.get(`/fullHostsDbList/${telegramId}`)
+    return await api.get(`/monitoring/fullHostsDbList/${telegramId}`)
   }
 
   public static async createConnection(connection) {
