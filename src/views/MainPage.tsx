2e45d6fe680dbd29a9a9a6cb03ee67b6b2e9ece0
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-11-04 18:48:07 +0500

    fix build

--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -12,7 +12,6 @@ import TypeMarker from "../components/ui/TypeMarker/TypeMarker";
 import { MdMoreVert } from 'react-icons/md'
 import { EnTypeLogEnum } from "../components/ui/enums/enTypeLog.enum";
 import HostActionSelect from "../components/HostActionSelect/HostActionSelect";
-import motionProps from '../constants/motion';
 
 const MainPage = () => {
   const [dbs, setDbs] = useState<Array<IDatabaseHost>>([]);
